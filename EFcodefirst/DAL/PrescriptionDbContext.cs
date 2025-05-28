efa492d6cbec1a085d6804a8c7475bf957168cc9
EwelinaPaczkowska <s30995@pjwstk.edu.pl>
2025-05-28 12:16:40 +0200

    added migrations

---
+++ EFcodefirst/DAL/PrescriptionDbContext.cs
@@ -0,0 +1,28 @@
+﻿using EFcodefirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFcodefirst.DAL;
+
+public class PrescriptionDbContext : DbContext
+{
+    public DbSet<Medicament> Medicament { get; set; }
+    public DbSet<Prescription_Medicament> Prescription_Medicament { get; set; }
+    public DbSet<Prescription> Prescription { get; set; }
+    public DbSet<Patient> Patient { get; set; }
+    public DbSet<Doctor> Doctor { get; set; }
+
+    protected PrescriptionDbContext()
+    {
+    }
+
+    public PrescriptionDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Prescription_Medicament>()
+            .HasKey(p => new { p.IdMedicament, p.IdPrescription });
+        
+    }
+}
--- EFcodefirst/Program.cs
+++ EFcodefirst/Program.cs
@@ -1,3 +1,9 @@
+using EFcodefirst.DAL;
+using EFcodefirst.Middlewares;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 namespace EFcodefirst;
 
 public class Program
@@ -5,31 +11,75 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        
+        string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-        // Add services to the container.
         builder.Services.AddAuthorization();
+        builder.Services.AddControllers();
+        
+        builder.Services.AddDbContext<PrescriptionDbContext>(opt =>
+        {
+            opt.UseSqlServer(connectionString)
+                .EnableSensitiveDataLogging()
+                .LogTo(Console.WriteLine, LogLevel.Information);
+        });
+        
+        
+        //builder.Services.AddScoped<IPrescriptionService, PrescriptionService>();
+        
+        
 
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "PrescriptionApi",
+                Description = "api for managing prescriptions",
+                Contact = new OpenApiContact
+                {
+                    Name="Api Support",
+                    Email="apiSupport@gmail.com",
+                    Url = new Uri("https://github.com/apiSupport")
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "MIT License",
+                    Url = new Uri("https://opensource.org/licenses/MIT")
+                }
+            });
+        });
+
+        //builder.Services.AddOpenApi();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            //app.MapOpenApi();
         }
+
+        app.UseGlobalExceptionHandling();
+        
+        app.UseSwagger();
+        
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "webTripApi");
+            
+            c.DocExpansion(DocExpansion.List);
+            c.DefaultModelExpandDepth(0);
+            c.DisplayRequestDuration();
+            c.EnableFilter();
+            
+        });
+        
 
         app.UseHttpsRedirection();
 
         app.UseAuthorization();
 
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+        app.MapControllers();
 
         app.Run();
     }
